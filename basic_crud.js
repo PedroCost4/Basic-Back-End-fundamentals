bb54134089192630f4479cef5ffaa3c950d64e78
PedroCost4 <pedro.costacdc@gmail.com>
2022-07-19 12:51:19 -0300

    fix

--- basic_crud.js
+++ basic_crud.js
@@ -1,51 +1,39 @@
 const DataBase = [];
 
-const insert = (id ,name,idade, email, cpf, admin) => {
-    const user = {};
-    user.id = id;
-    user.name = name;
-    user.idade = idade;
-    user.email = email;
-    user.cpf = cpf;
-    user.isAdmin = admin;
+const InsertUser = (name,age, email, cpf,passwd, admin) => {
+    const user = {
+    id : DataBase.length,
+    name: name,
+    age:age,
+    email:email,
+    cpf: cpf,
+    password: passwd,
+    isAdmin: admin
+    };
     return DataBase.push(user);
-}
+};
 
 const searchUser = (userData) => {
     let filteredDataBase = DataBase.filter((user => user.id === userData || user.cpf === userData),[])
     return filteredDataBase;
-}
+};
 
 const searchUserwithTargetData = (userData,wantedData) => {
-    let filteredDataBase = DataBase.filter((user => user.id === userData || user.cpf === userData),[])
+    let filteredDataBase = DataBase.filter((user => user.id === userData || user.cpf === userData), "User Not Found")
     return filteredDataBase[0][wantedData];
-}
+};
 
-const update = (id, element, newelement) => {
-    DataBase
-    .forEach(user => {
-            if (Object.values(user).includes(id)){
-                if (user.cpf == element){
-                    user.cpf = newelement;
-                }
-                else if (user.idade == element){
-                    user.idade = newelement;
-                }
-                else if (user.name == element){
-                    user.name = newelement;
-                }
-                else if (user.email == element){
-                    user.email = newelement;
-            }
-        }
-    })
-}
+const update = (id,data) => {
+    const idx = DataBase.findIndex((user) => user.id === id);
+  if (idx === -1) throw new Error('User not found');
+  DataBase[idx] = { ...DataBase[idx], ...data };
+};
 
 const deleteSingleData = (idLogin,targetId,data) => {
     if (DataBase.some(user => user.id == idLogin)){
     DataBase
     .forEach(user => {
-        if (Object.values(user).includes(targetId)){
+        if (user.id == targetId){
             delete user[data];
         }
     })
@@ -56,29 +44,25 @@ const deleteUser = (idLogin,targetId) => {
     DataBase
     .forEach(user => {
         if (user.id == idLogin && user.isAdmin == true){
-            DataBase.forEach(user => {
-                if (Object.values(user).includes(targetId)){
-                    DataBase.splice(DataBase.indexOf(user),1);      
-                }
-            })
+                delete[targetId]
         }
     })
-}
+ };
 
-const FilterNoAndAdminUsers = (AdminCond) => {
+const FilterNoAndAdminDataBase = (AdminCond) => {
     let filteredDataBase = DataBase.filter((user => user.isAdmin === AdminCond),[]);
     return filteredDataBase;
-}
+};
 
-insert(0, "Pedro", 65 , "pedro@gmail.com", "423.456.789-12", true);
-insert(1,"Lucas",54, "lucas@gmail.com", "345.645.786-21", false);
-insert(2,"João",32, "joao@gmail.com", "987.654.321-43", false);
-insert(3, "Bruno",78,"bruno@gmail.com", "567.345.789-21", true);
-console.log(DataBase);
+InsertUser( "Pedro", 65 , "pedro@gmail.com", "423.456.789-12", "-", true);
+InsertUser("Lucas",54, "lucas@gmail.com", "345.645.786-21","edrfgji09435", false);
+InsertUser("João",32, "joao@gmail.com", "987.654.321-43","ergfnjk8543" ,false);
+InsertUser( "Bruno",78,"bruno@gmail.com", "567.345.789-21","drfjiko76", true);
+console.table(DataBase);
 
-update(0, "Pedro", "Pedrito");
-update(1, "lucas@gmail.com", "lucas_js@gmail.com");
-console.log(DataBase);
+update(0,{name:"Pedrito",cpf:"123.321.765-98",password:"sdrfgnjou567"});
+update(1, {email:"lucas_js@gmail.com"});
+console.table(DataBase);
 
 deleteSingleData(0, 1, "cpf");
 console.log(DataBase);
@@ -86,13 +70,11 @@ console.log(DataBase);
 deleteUser(0,1);
 console.log(DataBase);
 
-const filtered = FilterNoAndAdminUsers(true);
+const filtered = FilterNoAndAdminDataBase(true);
 console.log(filtered);
 
 const filtered2 = searchUser(2);
 console.log(filtered2);
 
-const filteredwithTarget = searchUserwithTargetData("567.345.789-21", "name");
+const filteredwithTarget = searchUserwithTargetData(3, "name");
 console.log(filteredwithTarget);
-
-
