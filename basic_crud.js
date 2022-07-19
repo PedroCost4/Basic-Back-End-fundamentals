536517e9c6dc388474a94723c250ae629a282259
PedroCost4 <pedro.costacdc@gmail.com>
2022-07-19 09:51:03 -0300

    new funcs

--- basic_crud.js
+++ basic_crud.js
@@ -1,4 +1,4 @@
-const bd = []
+const DataBase = []
 
 const insert = (id ,name,idade, email, cpf, admin) => {
     const user = {}
@@ -8,11 +8,17 @@ const insert = (id ,name,idade, email, cpf, admin) => {
     user.email = email
     user.cpf = cpf
     user.isAdmin = admin
-    return bd.push(user)
+    return DataBase.push(user)
 }
 
+const searchUser = (userId,userCpf) => {
+    let filteredDataBase = DataBase.filter((user => user.id === userId || user.cpf === userCpf),[])
+    return filteredDataBase
+}
+
+
 const update = (id, element, newelement) => {
-    bd
+    DataBase
     .forEach(user => {
             if (Object.values(user).includes(id)){
                 if (user.cpf == element){
@@ -32,8 +38,8 @@ const update = (id, element, newelement) => {
 }
 
 const deleteSingleData = (idLogin,targetId,data) => {
-    if (bd.some(user => user.id == idLogin)){
-    bd
+    if (DataBase.some(user => user.id == idLogin)){
+    DataBase
     .forEach(user => {
         if (Object.values(user).includes(targetId)){
             delete user[data]
@@ -43,31 +49,43 @@ const deleteSingleData = (idLogin,targetId,data) => {
 }
 
 const deleteUser = (idLogin,targetId) => {
-    bd
+    DataBase
     .forEach(user => {
         if (user.id == idLogin && user.isAdmin == true){
-            bd.forEach(user => {
+            DataBase.forEach(user => {
                 if (Object.values(user).includes(targetId)){
-                    bd.splice(bd.indexOf(user),1)      
+                    DataBase.splice(DataBase.indexOf(user),1)      
                 }
             })
         }
     })
 }
 
+const FilterNoAndAdminUsers = (AdminCond) => {
+    let filteredDataBase = DataBase.filter((user => user.isAdmin === AdminCond),[])
+    return filteredDataBase
+}
+
 
 insert(0, "Pedro", 65 , "pedro@gmail.com", "423.456.789-12", true)
 insert(1,"Lucas",54, "lucas@gmail.com", "345.645.786-21", false)
 insert(2,"João",32, "joao@gmail.com", "987.654.321-43", false)
-console.log(bd)
+insert(3, "Bruno",78,"bruno@gmail.com", "567.345.789-21", true)
+console.log(DataBase)
 
 update(0, "Pedro", "Pedrito")
 update(1, "lucas@gmail.com", "lucas_js@gmail.com")
-console.log(bd)
+console.log(DataBase)
 
 deleteSingleData(0, 1, "cpf")
-console.log(bd)
+console.log(DataBase)
 
 deleteUser(0,1)
-console.log(bd)
+console.log(DataBase)
 
+const filtered = FilterNoAndAdminUsers(true)
+console.log(filtered)
+
+
+const filtered2 = searchUser(null, "567.345.789-21")
+console.log(filtered2)
