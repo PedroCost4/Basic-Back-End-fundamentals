9b6d9f4411ab451fe2724bb4fa56a4c2ef39e885
Pedro Costa <pedroha.costa1@gmail.com>
2022-07-26 15:02:40 -0300

    Create basic_crus2.js

---
+++ basic_crus2.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Home {
+    constructor(address,price,residents) {
+        this.address = address;
+    }
+}
